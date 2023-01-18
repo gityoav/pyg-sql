a991416abaa400588460f754da702150653cdfcb
Yoav Git <yoav.git@gmail.com>
2023-01-18 21:39:24 +0000

    path parsers for sql writers

--- src/pyg_sql/_sql_writer.py
+++ src/pyg_sql/_sql_writer.py
@@ -1,15 +1,73 @@
 from pyg_base import cache, Dict, is_pd, is_arr, is_dict, dictable, cfg_read
-from pyg_sql._sql_table import sql_table, _pairs2connection, _schema, _database, get_server
+from pyg_sql._sql_table import sql_table, _pairs2connection, _schema, _database, get_server, _types
 from pyg_encoders import encode, cell_root, root_path, root_path_check, dictable_decode, WRITERS
+
 import pandas as pd
 import pickle
+import re
+from pyg_base._bitemporal import _asof 
 
 sql_table_ = cache(sql_table)
 _sql = '.sql'
+_ts = '.ts'
 _dictable = '.dictable'
 _dictable_decode = encode(dictable_decode)
 _key = 'key'
 _data = 'data'
+_date = 'date'
+_variable_name = re.compile('^[A-Za-z]+[A-Za-z0-9_]*')
+
+
+def _parse_variable_name(key):
+    """
+    >>> key = 'param_49(date)'
+    >>> 
+    """
+    name = _variable_name.search(key).group(0)
+    return name
+
+def _parse_variable_type(key, name):
+    remain = key[len(name):]
+    if remain.startswith('('):
+        return _types[remain[1:].split(')')[0].lower()]
+    else:
+        return _types[str]
+
+def _parse_root(root):
+    if '.' in root:
+        suffix = root.split('.')[-1]
+        root = root[: -1 - len(suffix)]
+    else:
+        suffix = None
+    keys = root.split('%')[1:]
+    names = [_parse_variable_name(key) for key in keys]
+    types = [_parse_variable_type(key, name) for key, name in zip(keys, names)]
+    columns = dict(zip(names, types))
+    root = '/'.join(['%' + name for name in names])
+    return Dict(root = root, columns = columns, suffix = suffix)
+
+
+def _parse_path(path):
+    params = []
+    ps = path.split('/')
+    if len(ps) < 5:
+        raise ValueError('%s must have at least five items: server/database/schema/table/root'%path)
+    for i in range(len(ps)):
+        if '?' in ps[i]:
+            ps[i], prm = ps[i].split('?')
+            params.extend(prm.split('&'))            
+    connections = Dict(_pairs2connection(*params))
+    server, db, schema, table = ps[:4]
+    root = '/'.join(ps[4:])
+    server = get_server(server or connections.pop('server',None))
+    db = _database(db or connections.pop('db',None))
+    schema = _schema(schema or connections.pop('schema', None))
+    doc = connections.pop('doc', 'true')
+    doc = dict(true = True, false = False).get(doc.lower(), doc)        
+    return connections + dict(doc = doc, schema = schema, db = db, server = server, root = root, table = table,
+                              path = '%s/%s/%s/%s/%s'%(server, db, schema, table, root))
+    
+
 
 def sql_binary_store(path):
     """
@@ -39,27 +97,11 @@ def sql_binary_store(path):
     dict
         various connection parameters. specifically, the cursor parameter actually generates the table
     """
-    params = []
-    ps = path.split('/')
-    if len(ps) < 5:
-        raise ValueError('%s must have at least five items: server/database/schema/table/root'%path)
-    for i in range(len(ps)):
-        if '?' in ps[i]:
-            ps[i], prm = ps[i].split('?')
-            params.extend(prm.split('&'))            
-    connections = _pairs2connection(*params)
-    server, db, schema, table = ps[:4]
-    root = '/'.join(ps[4:])
-    server = get_server(server or connections.pop('server',None))
-    db = _database(db or connections.pop('db',None))
-    schema = _schema(schema or connections.pop('schema', None))
-    doc = connections.pop('doc', 'true')
-    doc = dict(true = True, false = False).get(doc.lower(), doc)        
-    cursor = sql_table(table = table, db = db, schema = schema, pk = _key, server = server, 
-                        non_null = {_data : bin}, doc = doc)
-    connections.update(dict(cursor = cursor, path = '%s/%s/%s/%s/%s'%(server, db, schema, table, root),
-                            server = server, schema = schema, db = db, table = table, root = root ))
-    return Dict(connections)
+    args = _parse_path(path)
+    args.cursor = sql_table(table = args.table, db = args.db, schema = args.schema, 
+                       pk = _key, server = args.server, 
+                       non_null = {_data : bin}, doc = args.doc)
+    return args
 
 
 def sql_dumps(obj, path):
